58b2dbd57343fe50fc1d616a1c55dbb732ff9789
Matias Insaurralde <matias@insaurral.de>
2018-06-19 03:39:28 -0400

    Use pkg-config in build.rs

--- build.rs
+++ build.rs
@@ -1,21 +1,24 @@
-use std::env;
 extern crate cc;
+extern crate pkg_config;
 
 fn main() {
-    let v8_include_path = env::var("V8_INCLUDE").unwrap();
-    let v8_build_path = env::var("V8_BUILD").unwrap();
+    let lib = pkg_config::probe_library("v8.pc");
+    if lib.is_err() {
+        panic!("Couldn't find V8 via pkg-config");
+    }
+    let v8 = lib.unwrap();
+    let incl_path = v8.include_paths[0].to_str().unwrap();
+    let lib_path = str::replace(incl_path, "v8/include", "out/v8build/obj");
 
-    cc::Build::new()
-        .cpp(true)
+    let mut build = cc::Build::new();
+    build.cpp(true)
         .cpp_link_stdlib("c++")
         .warnings_into_errors(true)
         .warnings(false)
-        .include(v8_include_path)
+        .include(incl_path)
         .flag("-std=c++11")
         .file("src/binding.cc")
         .compile("binding");
-
-    
-    println!("cargo:rustc-link-search=native={}", v8_build_path);
+    println!("cargo:rustc-link-search=native={}", lib_path);
     println!("cargo:rustc-link-lib=static=v8_monolith");
 }
